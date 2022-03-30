43e32761d0dfbea0c2868f3ed2b9fdd50d423354
kostenkoivan <bonanzaboss@gmail.com>
2022-03-30 15:17:02 +0300

    Задание по BasicSyntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,22 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    str.replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, function (i) {
+        result += {
+            M: 1000,
+            CM: 900,
+            D: 500,
+            CD: 400,
+            C: 100,
+            XC: 90,
+            L: 50,
+            XL: 40,
+            X: 10,
+            IX: 9,
+            V: 5,
+            IV: 4,
+            I: 1,
+        }[i];
+    });
     return result;
 }
